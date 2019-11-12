28709cc53d386549e1d7b18dc7b985626013877c
Arimateia Jr <arimateia@outlook.com>
2019-11-12 11:42:48 -0300

    commit inicial (migrando do bitbucket para o gitea)

---
+++ NuvemFiscalServidor/NuvemFiscalServidorVcl.dpr
@@ -0,0 +1,27 @@
+program NuvemFiscalServidorVcl;
+
+{$WARN DUPLICATE_CTOR_DTOR OFF}
+
+uses
+  Vcl.Forms,
+  Server.MainForm in 'Server.MainForm.pas' {ServerMainForm},
+  Server.Container in 'Server.Container.pas' {ServerContainer: TDataModule},
+  Server.Config in 'Server.Config.pas',
+  Common.Exceptions in 'common\Common.Exceptions.pas',
+  NFCe.DTO in 'nfce\NFCe.DTO.pas',
+  NFCe.Emissor in 'nfce\NFCe.Emissor.pas',
+  NFCe.Service.Interfaces in 'nfce\NFCe.Service.Interfaces.pas',
+  NFCe.Service in 'nfce\NFCe.Service.pas',
+  Config.Service.Interfaces in 'config\Config.Service.Interfaces.pas',
+  Config.DTO in 'config\Config.DTO.pas',
+  Config.Service in 'config\Config.Service.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TServerContainer, ServerContainer);
+  Application.CreateForm(TServerMainForm, ServerMainForm);
+  Application.Run;
+end.
